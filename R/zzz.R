69792776c90811464e13e55eb54a533c4bc57a32
Henrik Bengtsson <hb@aroma-project.org>
2010-04-18 11:42:57 +0000

    Version: 0.2.1 [2010-04-05] o Added a startup message when package is loaded. o CLEAN UP: Removed obsolete internal .First.lib() and .Last.lib().
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/matrixstats/pkg/matrixStats@49 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,5 @@
-.First.lib <- function(libname, pkgname) {
-  library.dynam(pkgname, package=pkgname)
+.onAttach <- function(libname, pkgname) {
+  pd <- utils::packageDescription(pkgname);
+  packageStartupMessage(pkgname, " v", pd$Version, " (", 
+    pd$Date, ") successfully loaded. See ?", pkgname, " for help."); 
 }
-
-.Last.lib <- function(libpath) {
-  library.dynam.unload(libpath=libpath)
-}
-
-
-
-
