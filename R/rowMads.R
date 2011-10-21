dc17b49fba91c834efcd9ea3f54855fb516743a0
Henrik Bengtsson <hb@aroma-project.org>
2011-10-21 15:07:29 +0000

    Added some forgotten HISTORY.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/matrixstats/pkg/matrixStats@56 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/rowMads.R
+++ R/rowMads.R
@@ -7,14 +7,14 @@ rowMads <- function(x, centers=NULL, constant=1.4826, ...) {
   # This is not actually necessary. It would be re-cycled
   # for 'x - centers' correctly.
   # But it enforces that 'x' must be a matrix.
-  ncol <- ncol(x)
-  centers <- matrix(rep(centers, times=ncol), ncol=ncol)
+  ncol <- ncol(x);
+  centers <- matrix(rep(centers, times=ncol), ncol=ncol);
   x <- x - centers;
   x <- abs(x);
   x <- rowMedians(x, ...);
   x <- constant*x;
   x;
-}
+} # rowMads()
 
 # HJ
 colMads <- function(x, centers=NULL, constant=1.4826, ...) {
@@ -26,17 +26,22 @@ colMads <- function(x, centers=NULL, constant=1.4826, ...) {
   # This is absolutely necessary! It would be re-cycled
   # for 'x - centers' incorrectly.
   # It also enforces that 'x' must be a matrix.
-  nrow <- nrow(x)
-  centers <- matrix(rep(centers, each=nrow), nrow=nrow)
+  nrow <- nrow(x);
+  centers <- matrix(rep(centers, each=nrow), nrow=nrow);
   x <- x - centers;
   x <- abs(x);
   x <- colMedians(x, ...);
   x <- constant*x;
   x;
-}
+} # colMads()
 
 ############################################################################
 # HISTORY:
+# 2011-10-13 [HJ]
+# o Implemented colMads() as rowMads() by using the improved colMedians().
+# o Now rowMads() expands 'center' to a matrix of the same dimensions as
+#   'x'.  This is not actually necessary, but it enforces that 'x' must be
+#   a matrix.
 # 2008-03-26 [HB] 
 # o Created.
 ############################################################################
