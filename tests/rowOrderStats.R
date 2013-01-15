a9264ad1f2b18371e1f8e46e8bfc5a535ab94581
Henrik Bengtsson <hb@aroma-project.org>
2013-01-15 04:07:33 +0000

    Made system test for rowOrderStats() a bit faster for CRAN.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/matrixstats/pkg/matrixStats@125 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tests/rowOrderStats.R
+++ tests/rowOrderStats.R
@@ -7,6 +7,8 @@ set.seed(1)
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 cat("Benchmarking:\n")
 
+K <- if (Sys.getenv("_R_CHECK_FULL_") == "") 5 else 3
+
 # Simulate data in a matrix of any shape
 nrow <- 1000
 ncol <- 1000
@@ -16,12 +18,12 @@ probs <- 0.3
 which <- round(probs*ncol)
 gc()
 t0 <- system.time({
-  for (rr in 1:5)
+  for (rr in 1:K)
     y0 <- apply(x, MARGIN=1, FUN=quantile, probs=probs, type=3)
 })
 gc()
 t1 <- system.time({
-  for (rr in 1:5)
+  for (rr in 1:K)
     y1 <- rowOrderStats(x, which=which)
 })
 stopifnot(all.equal(y0,y1))
@@ -32,7 +34,7 @@ cat(sprintf("rowOrderStats()/apply(): %.3g\n", (t1/t0)[3]))
 # Consistency checks
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 cat("Consistency checks without NAs:\n")
-for (kk in 1:5) {
+for (kk in 1:K) {
   cat("Random test #", kk, "\n", sep="")
 
   # Simulate data in a matrix of any shape
