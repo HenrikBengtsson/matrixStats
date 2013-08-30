a2c004fdc8715b9b79c4fb6a9c186f881b20df96
Henrik Bengtsson <hb@aroma-project.org>
2013-08-30 20:48:21 +0000

    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/matrixstats/pkg/matrixStats@164 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname) {
   pd <- utils::packageDescription(pkgname);
-  startupMessage(pkgname, " v", pd$Version, " (",
+  pkgStartupMessage(pkgname, " v", pd$Version, " (",
     pd$Date, ") successfully loaded. See ?", pkgname, " for help.");
 }
