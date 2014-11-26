0bd0dcc4b5c540b04dc239cb8118bed21890e1d7
Bengtsson <hb@biostat.ucsf.edu>
2014-11-25 19:52:30 -0800

    Tweaks to Rd

--- R/x_OP_y.R
+++ R/x_OP_y.R
@@ -1,10 +1,14 @@
 ############################################################################/**
 # @RdocFunction x_OP_y
+# @alias x_OP_y
 # @alias t_tx_OP_y
 #
 # @title "Fast calculation of 'z <- x OP y' and 'z <- t(t(x) OP y'"
 #
-# @synopsis
+# \usage{
+#  @usage x_OP_y
+#  @usage t_tx_OP_y
+# }
 #
 # \description{
 #   @get "title", where OP can be +, -, *, and /.
@@ -15,12 +19,21 @@
 #   \item{x}{A @numeric NxK @matrix.}
 #   \item{y}{A @numeric @vector of length L.}
 #   \item{FUN}{A @character specifying which operator to use.}
-#   \item{na.rm}{If @TRUE, missing values are skipped, otherwise not.}
+#   \item{na.rm}{If @TRUE, missing values are ignored, otherwise not.}
 #   \item{...}{Not used.}
 # }
 #
 # \value{
 #   Returns a @numeric NxK @matrix.
+# }
+#
+# \section{Missing values}{
+#   If \code{na.rm=TRUE}, then missing values are "dropped" before applying
+#   the operator to each pair of values.  For instance, if \code{x[1,1]} is
+#   a missing value, then the result of \code{x[1,1] + y[1]} equals
+#   \code{y[1]}.  If also \code{y[1]} is a missing value, then the result
+#   is a missing value.  This only applies to additions and multiplications.
+#   For subtractions and divisions, argument \code{na.rm} is ignored.
 # }
 #
 # @examples "../incl/x_OP_y.Rex"
