81feadefb4c5a1a0cb697eafb8b282adea851220
hb <hb@aroma-project.org>
2014-12-07 00:13:46 -0800

    BUG FIX: product(x, na.rm=FALSE) for integer 'x' with missing values gave garbage

--- src/productExpSumLog_TYPE-template.h
+++ src/productExpSumLog_TYPE-template.h
@@ -33,30 +33,35 @@ LDOUBLE METHOD_NAME(X_C_TYPE *x, R_xlen_t nx, int narm, int hasna) {
   /* Calculate sum(log(abs(x))) */
   for (ii = 0 ; ii < nx; ii++) {
     t = x[ii];
-    /* Skip missing values? */
+    /* Missing values? */
     if (narm) {
       if (X_ISNAN(t)) continue;
     }
 
 #if X_TYPE == 'i'
     /* Early stopping? */
-    if (t < 0) {
+    if (X_ISNAN(t)) {
+      y = NA_REAL;
+      break;
+    } else if (t < 0) {
       isneg = !isneg;
     } else if (t == 0) {
       y = R_NegInf;
       break;
     }
 #elif X_TYPE == 'r'
-    if (t < 0) isneg = !isneg;
+    if (t < 0) {
+      isneg = !isneg;
+    }
 #endif
-    t = X_ABS(t);
+    t = fabs(t);
     t = log(t);
     y += t;
     /*
       Rprintf("#%d: x=%g, is.nan(x)=%d, abs(x)=%g, is.nan(abs(x))=%d, log(abs(x))=%g, is.nan(log(abs(x)))=%d, sum=%g, is.nan(sum)=%d\n", ii, x[ii], R_IsNaN(x[ii]), X_ABS(x[ii]), R_IsNaN(abs(x[ii])), t, R_IsNaN(y), y, R_IsNaN(y));  */
   }
 
-  if (X_ISNAN(y)) {
+  if (ISNAN(y)) {
     /* If there where NA and/or NaN elements, then 'y' will at this
        point be NaN. The information on an NA value is lost when 
        calculating fabs(NA), which returns NaN. For consistency with
--- tests/logSumExp.R
+++ tests/logSumExp.R
@@ -10,15 +10,15 @@ for (mode in c("integer", "numeric")) {
   x <- runif(20, min=1.0, max=3.0)
   storage.mode(x) <- mode
   str(x)
-  
+
   ## The logarithm of the harmonic mean
   y0 <- log(1/mean(1/x))
   print(y0)  ## -1.600885
-  
+
   lx <- log(x)
   y1 <- log(length(x)) - logSumExp(-lx)
   print(y1)  ## [1] -1.600885
-  
+
   # Sanity check
   stopifnot(all.equal(y1, y0))
 
