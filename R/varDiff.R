c7b99eac066725cc3f27bbd1b9714134a8897753
hb <hb@aroma-project.org>
2015-01-16 08:03:21 -0800

    Moved sdDiff() and madDiff() into varDiff.R

--- R/varDiff.R
+++ R/varDiff.R
@@ -96,6 +96,78 @@ varDiff <- function(x, na.rm=FALSE, diff=1L, trim=0, ...) {
 } # varDiff()
 
 
+sdDiff <- function(x, na.rm=FALSE, diff=1L, trim=0, ...) {
+  if (na.rm)
+    x <- x[!is.na(x)];
+
+  # Nothing to do?
+  n <- length(x);
+  if (n == 0L)
+    return(NA_real_);
+
+  # Calculate differences?
+  if (diff > 0L) {
+    x <- diff2(x, differences=diff);
+
+    # Nothing to do?
+    n <- length(x);
+    if (n == 0L)
+      return(NA_real_);
+  }
+
+  # Trim?
+  if (trim > 0 && n > 0L) {
+    if (anyMissing(x)) return(NA_real_);
+    lo <- floor(n*trim)+1;
+    hi <- (n+1)-lo;
+    partial <- unique(c(lo, hi))
+    x <- sort.int(x, partial=partial);
+    x <- x[lo:hi];
+  }
+
+  # Estimate
+  sd <- sd(x, na.rm=FALSE);
+  x <- NULL; # Not needed anymore
+  sd/(sqrt(2)^diff);
+} # sdDiff()
+
+
+madDiff <- function(x, na.rm=FALSE, diff=1L, trim=0, ...) {
+  if (na.rm)
+    x <- x[!is.na(x)];
+
+  # Nothing to do?
+  n <- length(x);
+  if (n == 0L)
+    return(NA_real_);
+
+  # Calculate differences?
+  if (diff > 0L) {
+    x <- diff2(x, differences=diff);
+
+    # Nothing to do?
+    n <- length(x);
+    if (n == 0L)
+      return(NA_real_);
+  }
+
+  # Trim?
+  if (trim > 0 && n > 0L) {
+    if (anyMissing(x)) return(NA_real_);
+    lo <- floor(n*trim)+1;
+    hi <- (n+1)-lo;
+    partial <- unique(c(lo, hi))
+    x <- sort.int(x, partial=partial);
+    x <- x[lo:hi];
+  }
+
+  # Estimate
+  sd <- mad(x, na.rm=FALSE, ...);
+  x <- NULL; # Not needed anymore
+  sd / (sqrt(2)^diff);
+} # madDiff()
+
+
 rowVarDiffs <- function(x, na.rm=FALSE, diff=1L, trim=0, ...) {
   apply(x, MARGIN=1L, FUN=varDiff, na.rm=na.rm, diff=diff, trim=trim, ...)
 } # rowVarDiffs()
@@ -128,19 +200,19 @@ colMadDiffs <- function(x, na.rm=FALSE, diff=1L, trim=0, ...) {
 # 2014-14-19 [HB]
 # o Added (col|row)(Var|Sd|Mad)Diffs() for completeness.
 # 2014-11-10
-# o Turned varDiff() into a function.
+# o Turned *Diff() into a function.
 # 2014-05-24
-# o Turned varDiff() into an S3 method (was S4).
+# o Turned *Diff() into an S3 method (was S4).
 # 2014-04-26
 # o Added argument 'trim' to madDiff(), sdDiff() and varDiff().
 # 2013-11-23
-# o MEMORY: Now varDiff() cleans out allocated objects sooner.
+# o MEMORY: Now *Diff() cleans out allocated objects sooner.
 # 2012-07-17
 # o Added the reference to von Neumann et al. (1941).
 # 2009-02-02
 # o Added Rdoc comments.
 # 2008-04-13
-# o Added varDiff().
+# o Added varDiff(), sdDiff() and madDiff().
 # 2008-04-10 [on UA930 SFO-LHR]
 # o Created.
 ############################################################################
