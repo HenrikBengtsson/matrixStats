447dc1668e598ea695d37d01728fe16c3c88190f
hb <hb@aroma-project.org>
2015-01-22 21:15:20 -0800

    TESTS: Adding tests for loading/unloading package.

---
+++ tests/zzz.package-unload.R
@@ -0,0 +1,24 @@
+## These tests need to be last of all tests, otherwise
+## covr::package_coverage() gives an error.
+cat("1. Loading package\n")
+requireNamespace("matrixStats")
+stopifnot("matrixStats" %in% loadedNamespaces())
+
+cat("2. Unloading package\n")
+unloadNamespace("matrixStats")
+stopifnot(!"matrixStats" %in% loadedNamespaces())
+
+cat("3. Attaching package\n")
+library("matrixStats")
+stopifnot("package:matrixStats" %in% search())
+
+cat("3. Detaching package\n")
+detach("package:matrixStats")
+stopifnot(!"package:matrixStats" %in% search())
+stopifnot("matrixStats" %in% loadedNamespaces())
+
+cat("4. Unloading package\n")
+unloadNamespace("matrixStats")
+stopifnot(!"matrixStats" %in% loadedNamespaces())
+
+cat("5. DONE\n")
