9c788b6de990dce5d16daafb39128bea8feb8187
hb <hb@aroma-project.org>
2015-01-22 22:42:56 -0800

    TESTS: Retrying with official covr

--- tests/zzz.package-unload.R
+++ tests/zzz.package-unload.R
@@ -8,27 +8,33 @@ cat("2. Unloading package\n")
 unloadNamespace("matrixStats")
 stopifnot(!"matrixStats" %in% loadedNamespaces())
 
-cat("3. Attaching package\n")
-library("matrixStats")
-stopifnot("package:matrixStats" %in% search())
+if (TRUE) {
+  ## 'covr' gives "Error in library("matrixStats") :
+  ## there is no package called 'matrixStats'" here, cf.
+  ## https://travis-ci.org/HenrikBengtsson/matrixStats/builds/48015577
 
-cat("3. Detaching package\n")
-detach("package:matrixStats")
-stopifnot(!"package:matrixStats" %in% search())
-stopifnot("matrixStats" %in% loadedNamespaces())
+  cat("3. Attaching package\n")
+  library("matrixStats")
+  stopifnot("package:matrixStats" %in% search())
 
-cat("4. Unloading package\n")
-unloadNamespace("matrixStats")
-stopifnot(!"matrixStats" %in% loadedNamespaces())
+  cat("4. Detaching package\n")
+  detach("package:matrixStats")
+  stopifnot(!"package:matrixStats" %in% search())
+  stopifnot("matrixStats" %in% loadedNamespaces())
 
-cat("5. Attaching package (again)\n")
-library("matrixStats")
-stopifnot("package:matrixStats" %in% search())
+  cat("5. Unloading package\n")
+  unloadNamespace("matrixStats")
+  stopifnot(!"matrixStats" %in% loadedNamespaces())
 
-cat("6. Detaching package (again)\n")
-detach("package:matrixStats")
-stopifnot(!"package:matrixStats" %in% search())
-stopifnot("matrixStats" %in% loadedNamespaces())
+  cat("6. Attaching package (again)\n")
+  library("matrixStats")
+  stopifnot("package:matrixStats" %in% search())
+
+  cat("7. Detaching package (again)\n")
+  detach("package:matrixStats")
+  stopifnot(!"package:matrixStats" %in% search())
+  stopifnot("matrixStats" %in% loadedNamespaces())
+}
 
 cat("7. DONE\n")
 
