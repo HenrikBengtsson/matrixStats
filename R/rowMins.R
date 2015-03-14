6ffd99e76b9e0a80f49eb581a5c3829689d0d423
Dongcan Jiang <dongcan.jiang@gmail.com>
2015-03-14 14:26:52 +0800

    [GSoC/Test4] rowMins_C()
    Add rowMins_C() implemented in C which is a prototype of rowMins_R()
    function above using the inline package.
    
    This function is tested in tests/rowMins.R by comparing the results
    with rowMins(). See more details in tests/rowMins.R.
    
    Signed-off-by: Dongcan Jiang <dongcan.jiang@gmail.com>

--- R/rowMins.R
+++ R/rowMins.R
@@ -11,6 +11,8 @@
 # \usage{
 #  @usage rowMins_R
 #  @usage colMins_R
+#  @usage rowMins_C
+#  @usage colMins_C
 # }
 #
 # \arguments{
@@ -22,6 +24,11 @@
 #
 # \value{
 #   Returns a @vector of length N.
+# }
+#
+# \details{
+#   The implementation of \code{rowMins_C()} is optimized for both speed
+#   and memory by using C.
 # }
 #
 # @author "Dongcan Jiang"
@@ -43,3 +50,52 @@ colMins_R <- function(x, rows=1:dim(x)[1], ...) {
     apply(x[rows,], MARGIN=2L, FUN=min, ...)
   })
 }
+
+rowMins_C.init <- function() {
+  library(inline)
+  sigFunc <- signature(x="array", cols="array")
+  code <- '
+    int N = length(cols);
+    int M = INTEGER(GET_DIM(x))[0];
+    SEXP res;
+    if (isInteger(x)) PROTECT(res = allocVector(INTSXP, M));
+    else if (isReal(x)) PROTECT(res = allocVector(REALSXP, M));
+    else error("Only Integer and Real are supported.");
+    for (int i = 0; i < M; ++ i) {
+      int minValue_Integer;
+      double minValue_Real;
+      if (isInteger(x)) minValue_Integer = INTEGER(x)[i+M*(INTEGER(cols)[0]-1)];
+      else if (isReal(x)) minValue_Real = REAL(x)[i+M*(INTEGER(cols)[0]-1)];
+
+      for (int j = 1; j < N; ++ j) {
+        if (isInteger(x)) {
+          int value = INTEGER(x)[i+M*(INTEGER(cols)[j]-1)];
+          if (value < minValue_Integer) {
+            minValue_Integer = value;
+          }
+        } else if (isReal(x)) {
+          double value = REAL(x)[i+M*(INTEGER(cols)[j]-1)];
+          if (value < minValue_Real) {
+            minValue_Real = value;
+          }
+        }
+      }
+      if (isInteger(x)) INTEGER(res)[i] = minValue_Integer;
+      else if (isReal(x)) REAL(res)[i] = minValue_Real;
+    }
+    UNPROTECT(1);
+    return res;
+  '
+  rowMins_C_Inline <- cfunction(sigFunc, code)
+}
+
+rowMins_C <- function(x, cols=dim(x)[2], ...) {
+  rowMins_C_Inline <- rowMins_C.init()
+  rowMins_C_Inline(x, cols)
+}
+
+colMins_C <- function(x, rows=1:dim(x)[1], ...) {
+  # Not supported yet. Just use the R version.
+  colMins_R(x, rows)
+}
+
--- tests/rowMins.R
+++ tests/rowMins.R
@@ -1,10 +1,31 @@
 library("matrixStats")
+# When R CMD check runs tests, it sets R_TESTS. When the tests
+# themeselves run R CMD xxxx, as is the case with the tests in
+# devtools, having R_TESTS set causes errors because it confuses
+# the R subprocesses. Unsetting it here avoids those problems.
+Sys.setenv("R_TESTS" = "")
 
+# Test for Integer
 x <- matrix(1:100+0.1, nrow=20, ncol=5)
-
 m0 <- rowMins(x[,4:2], na.rm=1)
 m1 <- rowMins_R(x, 2:4)
 m2 <- colMins_R(t(x), 4:2)
-stopifnot(all.equal(m1, m0))
-stopifnot(all.equal(m1, m2))
+m3 <- rowMins_C(x, 4:2)
+m4 <- colMins_C(t(x), 2:4)
+stopifnot(all.equal(m0, m1))
+stopifnot(all.equal(m0, m2))
+stopifnot(all.equal(m0, m3))
+stopifnot(all.equal(m0, m4))
+
+# Test for Real
+x <- matrix(1:100, nrow=20, ncol=5)
+m0 <- rowMins(x[,4:2], na.rm=1)
+m1 <- rowMins_R(x, 2:4)
+m2 <- colMins_R(t(x), 4:2)
+m3 <- rowMins_C(x, 4:2)
+m4 <- colMins_C(t(x), 2:4)
+stopifnot(all.equal(m0, m1))
+stopifnot(all.equal(m0, m2))
+stopifnot(all.equal(m0, m3))
+stopifnot(all.equal(m0, m4))
 
