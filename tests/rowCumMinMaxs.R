0c80ffa76fe713b3ba170132da2e800cb0e2bda4
hb <hb@aroma-project.org>
2015-05-24 20:06:26 -0700

    Testing 1x1 matrix for integers and doubles

--- tests/rowCumMinMaxs.R
+++ tests/rowCumMinMaxs.R
@@ -31,15 +31,15 @@ colCummaxs_R <- function(x) {
 for (mode in c("integer", "double")) {
   for (addNA in c(FALSE, TRUE)) {
     cat("addNA=", addNA, "\n", sep="")
-  
+
     x <- matrix(1:100, nrow=20, ncol=5)
     if (addNA) {
       x[13:17,c(2,4)] <- NA_real_
     }
     cat("mode: ", mode, "\n", sep="")
-    storage.mode(x) <- mode 
+    storage.mode(x) <- mode
     str(x)
-  
+
     # Row/column ranges
     r0 <- rowCummins_R(x)
     r1 <- rowCummins(x)
@@ -47,7 +47,7 @@ for (mode in c("integer", "double")) {
     stopifnot(all.equal(r1, r2))
     stopifnot(all.equal(r1, r0))
     stopifnot(all.equal(r2, r0))
-  
+
     r0 <- rowCummaxs_R(x)
     r1 <- rowCummaxs(x)
     r2 <- t(colCummaxs(t(x)))
@@ -64,7 +64,7 @@ for (mode in c("integer", "double")) {
 for (mode in c("integer", "double")) {
   x <- matrix(NA_real_, nrow=20, ncol=5)
   cat("mode: ", mode, "\n", sep="")
-  storage.mode(x) <- mode 
+  storage.mode(x) <- mode
   str(x)
 
   r0 <- rowCummins_R(x)
@@ -73,7 +73,7 @@ for (mode in c("integer", "double")) {
   stopifnot(all.equal(r1, r2))
   stopifnot(all.equal(r1, r0))
   stopifnot(all.equal(r2, r0))
-  
+
   r0 <- rowCummaxs_R(x)
   r1 <- rowCummaxs(x)
   r2 <- t(colCummaxs(t(x)))
@@ -86,18 +86,23 @@ for (mode in c("integer", "double")) {
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # A 1x1 matrix
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-x <- matrix(0, nrow=1, ncol=1)
-r0 <- rowCummins_R(x)
-r1 <- rowCummins(x)
-r2 <- t(colCummins(t(x)))
-stopifnot(all.equal(r1, r2))
-stopifnot(all.equal(r1, r0))
-stopifnot(all.equal(r2, r0))
+for (mode in c("integer", "double")) {
+  x <- matrix(0, nrow=1, ncol=1)
+  cat("mode: ", mode, "\n", sep="")
+  storage.mode(x) <- mode
+  str(x)
 
-r0 <- rowCummaxs_R(x)
-r1 <- rowCummaxs(x)
-r2 <- t(colCummaxs(t(x)))
-stopifnot(all.equal(r1, r2))
-stopifnot(all.equal(r1, r0))
-stopifnot(all.equal(r2, r0))
+  r0 <- rowCummins_R(x)
+  r1 <- rowCummins(x)
+  r2 <- t(colCummins(t(x)))
+  stopifnot(all.equal(r1, r2))
+  stopifnot(all.equal(r1, r0))
+  stopifnot(all.equal(r2, r0))
 
+  r0 <- rowCummaxs_R(x)
+  r1 <- rowCummaxs(x)
+  r2 <- t(colCummaxs(t(x)))
+  stopifnot(all.equal(r1, r2))
+  stopifnot(all.equal(r1, r0))
+  stopifnot(all.equal(r2, r0))
+} # for (mode ...)
