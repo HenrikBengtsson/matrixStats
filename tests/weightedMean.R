42d77a6d1384632d38f77c39dae859d3d0194740
Dongcan Jiang <dongcan.jiang@gmail.com>
2015-05-31 10:19:26 +0800

    more complete subset tests for weightedMean
    Signed-off-by: Dongcan Jiang <dongcan.jiang@gmail.com>

--- tests/weightedMean.R
+++ tests/weightedMean.R
@@ -76,8 +76,15 @@ stopifnot(identical(m1,m0))
 # Subsetted tests
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 source("utils/validateIndicesFramework.R")
-x <- runif(6, min=-6, max=6)
-w <- runif(6, min=-6, max=6)
-for (idxs in indexCases) {
-  validateIndicesTestVector_w(x, w, idxs, ftest=weightedMean, fsure=weighted.mean)
+for (mode in c("numeric", "integer")) {
+  x <- runif(6, min=-6, max=6)
+  w <- runif(6, min=0, max=6)
+  storage.mode(x) <- mode
+  storage.mode(w) <- mode
+  if (mode == "numeric") w[1] <- Inf
+
+  for (idxs in indexCases) {
+    validateIndicesTestVector_w(x, w, idxs, ftest=weightedMean, fsure=weighted.mean, na.rm=TRUE)
+    validateIndicesTestVector_w(x, w, idxs, ftest=weightedMean, fsure=weighted.mean, na.rm=FALSE)
+  }
 }
