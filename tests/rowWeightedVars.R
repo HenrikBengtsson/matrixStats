2dfc15f2769b76f9ab3044e88ffac9355ba90b96
Dongcan Jiang <dongcan.jiang@gmail.com>
2015-05-31 19:37:51 +0800

    weightedMad.R: subsetted mockups and tests
    Signed-off-by: Dongcan Jiang <dongcan.jiang@gmail.com>

--- tests/rowWeightedVars.R
+++ tests/rowWeightedVars.R
@@ -105,3 +105,44 @@ print(xM1)
 stopifnot(all.equal(xM1, xM0))
 xM2 <- colWeightedMads(t(x), w=w)
 stopifnot(all.equal(xM2, xM0))
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Subsetted tests
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+cat("=== subsetted tests ===\n")
+
+FUNs <- list(
+  weightedVar=c(rowWeightedVars, colWeightedVars),
+  weightedSd=c(rowWeightedSds, colWeightedVars),
+  weightedMad=c(rowWeightedMads, colWeightedMads)
+)
+
+source("utils/validateIndicesFramework.R")
+for (fcn in names(FUNs)) {
+  # TODO: can remove it when all functions' subsetted features are supported
+  if (is.element(fcn, c("weightedVar", "weightedSd"))) next
+
+  cat(sprintf("subsetted tests on matrix %s()...\n", fcn))
+  rowFUN <- FUNs[[fcn]][[1]]
+  colFUN <- FUNs[[fcn]][[2]]
+
+  for (mode in c("numeric", "integer")) {
+    x <- matrix(runif(6*6, min=-6, max=6), nrow=6, ncol=6)
+    w <- runif(6, min=0, max=6)
+    storage.mode(x) <- mode
+    storage.mode(w) <- mode
+    if (mode == "numeric") w[1] <- Inf
+
+    for (rows in indexCases) {
+      for (cols in indexCases) {
+        for (na.rm in c(TRUE, FALSE)) {
+          validateIndicesTestMatrix_w(x, w, rows, cols, ftest=rowFUN, fsure=rowFUN, na.rm=na.rm)
+          validateIndicesTestMatrix_w(x, w, rows, cols, fcolTest=colFUN, fsure=rowFUN, na.rm=na.rm)
+        }
+      }
+    }
+  }
+
+  cat(sprintf("%s()...DONE\n", fcn))
+}
--- tests/weightedVar_etal.R
+++ tests/weightedVar_etal.R
@@ -74,3 +74,32 @@ for (fcn in names(FUNs)) {
 
   cat(sprintf("%s()...DONE\n", fcn))
 } # for (fcn ...)
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Subsetted tests
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+cat("=== subsetted tests ===\n")
+source("utils/validateIndicesFramework.R")
+for (fcn in names(FUNs)) {
+  # TODO: can remove it when all functions' subsetted features are supported
+  if (is.element(fcn, c("weightedVar", "weightedSd"))) next
+
+  cat(sprintf("subsetted tests on %s()...\n", fcn))
+  FUN <- FUNs[[fcn]]
+
+  for (mode in c("numeric", "integer")) {
+    x <- runif(6, min=-6, max=6)
+    w <- runif(6, min=0, max=6)
+    storage.mode(x) <- mode
+    storage.mode(w) <- mode
+    if (mode == "numeric") w[1] <- Inf
+
+    for (idxs in indexCases) {
+      validateIndicesTestVector_w(x, w, idxs, ftest=FUN, fsure=FUN, na.rm=TRUE)
+      validateIndicesTestVector_w(x, w, idxs, ftest=FUN, fsure=FUN, na.rm=FALSE)
+    }
+  }
+
+  cat(sprintf("%s()...DONE\n", fcn))
+}
