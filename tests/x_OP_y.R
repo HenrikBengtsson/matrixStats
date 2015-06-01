b92039a4eae0b983dc7ff1b2c9b1c22f4f15bb95
Dongcan Jiang <dongcan.jiang@gmail.com>
2015-06-01 20:31:29 +0800

    x_OP_y.R: subsetted mockups and tests
    Signed-off-by: Dongcan Jiang <dongcan.jiang@gmail.com>

--- tests/x_OP_y.R
+++ tests/x_OP_y.R
@@ -143,6 +143,56 @@ for (mode in c("integer", "double")) {
 } # for (mode ...)
 
 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Subsetted tests
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+source("utils/validateIndicesFramework.R")
 
+for (OP in c("+", "-", "*", "/")) {
+  for (mode in c("numeric", "integer")) {
+    x <- matrix(runif(6*6, min=-6, max=6), nrow=6L, ncol=6L)
+    y <- runif(6, min=0, max=6)
+    storage.mode(x) <- mode
+    storage.mode(y) <- mode
+    if (mode == "numeric") y[1] <- Inf
 
+    for (xrows in indexCases) {
+      for (xcols in indexCases) {
+        if (is.null(xrows)) xrows <- seq_len(nrow(x))
+        if (is.null(xcols)) xcols <- seq_len(ncol(x))
 
+        for (yidxs in list(xrows,xcols)) {
+          for (na.rm in c(TRUE, FALSE)) {
+            if (is.element(OP, c("-", "/"))) next
+
+            suppressWarnings({
+              actual <- tryCatch(
+                x_OP_y(x, y, OP, xrows=xrows, xcols=xcols, yidxs=yidxs, na.rm=na.rm),
+                error=function(c) "error"
+              )
+
+              expect <- tryCatch(
+                x_OP_y_R(x[xrows,xcols,drop=FALSE], y[yidxs], OP, na.rm=na.rm),
+                error=function(c) "error"
+              )
+            })
+            stopifnot(all.equal(as.vector(actual), as.vector(expect)))
+
+            suppressWarnings({
+              actual <- tryCatch(
+                t_tx_OP_y(x, y, OP, xrows=xrows, xcols=xcols, yidxs=yidxs, na.rm=na.rm),
+                error=function(c) "error"
+              )
+
+              expect <- tryCatch(
+                t_tx_OP_y_R(x[xrows,xcols,drop=FALSE], y[yidxs], OP, na.rm=na.rm),
+                error=function(c) "error"
+              )
+            })
+            stopifnot(all.equal(as.vector(actual), as.vector(expect)))
+          }
+        }
+      }
+    }
+  }
+}
