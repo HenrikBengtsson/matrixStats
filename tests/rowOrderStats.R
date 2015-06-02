b57309739d23dc802d0d8a8c354e25eef52e2ebc
Dongcan Jiang <dongcan.jiang@gmail.com>
2015-06-03 02:10:42 +0800

    rowOrderStats.R: subsetted mockups and tests
    Signed-off-by: Dongcan Jiang <dongcan.jiang@gmail.com>

--- tests/rowOrderStats.R
+++ tests/rowOrderStats.R
@@ -1,8 +1,12 @@
 library("matrixStats")
 library("stats")
 
-rowOrderStats_R <- function(x, probs) {
-  apply(x, MARGIN=1L, FUN=quantile, probs=probs, type=3L)
+rowOrderStats_R <- function(x, probs, ...) {
+  ans <- apply(x, MARGIN=1L, FUN=quantile, probs=probs, type=3L)
+
+  # Remove Attributes
+  attributes(ans) <- NULL
+  ans
 } # rowOrderStats_R()
 
 
@@ -54,3 +58,31 @@ for (mode in c("integer", "double")) {
     stopifnot(all.equal(y2,y0))
   } # for (kk in ...)
 } # for (mode ...)
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Subsetted tests
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+source("utils/validateIndicesFramework.R")
+x <- matrix(runif(6*6, min=-6, max=6), nrow=6, ncol=6)
+storage.mode(x) <- "integer"
+probs <- 0.3
+for (rows in indexCases) {
+  for (cols in indexCases) {
+
+    if (is.null(cols)) which <- round(probs*ncol(x))
+    else {
+      xxrows <- rows
+      suppressWarnings({
+        xx <- tryCatch(x[,cols,drop=FALSE], error=function(c) "error")
+        if (!identical(xx, "error")) {
+          which <- round(probs*ncol(xx))
+        }
+      })
+    }
+    if (which == 0) next
+
+    validateIndicesTestMatrix(x, rows, cols, ftest=rowOrderStats, fsure=rowOrderStats_R, which=which, probs=probs)
+    validateIndicesTestMatrix(x, rows, cols, fcolTest=colOrderStats, fsure=rowOrderStats_R, which=which, probs=probs)
+  }
+}
