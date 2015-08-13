1bff809f0eea07ddc30cacf866e1c3131655e2bd
Dongcan Jiang <dongcan.jiang@gmail.com>
2015-08-13 18:31:34 +0800

    debug rowRanks parallel

--- tests/rowRanks_parallel.R
+++ tests/rowRanks_parallel.R
@@ -15,10 +15,10 @@ x <- matrix(runif(6*6, min=-6, max=6), nrow=6, ncol=6)
 storage.mode(x) <- "integer"
 for (rows in indexCases) {
   for (cols in indexCases) {
-    validateIndicesTestMatrix(x, rows, cols, ftest=rowRanks, fsure=rowRanks_R, ties.method="average", mc.cores=2L)
+    validateIndicesTestMatrix(x, rows, cols, ftest=rowRanks, fsure=rowRanks_R, ties.method="average", mc.cores=2L, debug=TRUE)
 
     validateIndicesTestMatrix(x, rows, cols, ftest=function(x, rows, cols, ...) {
       t(colRanks(t(x), rows=cols, cols=rows, preserveShape=TRUE, ...))
-    }, fsure=rowRanks_R, ties.method="average", mc.cores=2L)
+    }, fsure=rowRanks_R, ties.method="average", mc.cores=2L, debug=TRUE)
   }
 }
