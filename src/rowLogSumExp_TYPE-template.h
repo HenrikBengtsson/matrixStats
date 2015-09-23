13e54d629a9525c4a7bc668bc0629cad457ad0de
hb <hb@aroma-project.org>
2015-09-23 14:40:08 -0700

    Forward porting from feature/subsetting [#70]

--- src/rowLogSumExp_TYPE-template.h
+++ src/rowLogSumExp_TYPE-template.h
@@ -3,7 +3,7 @@
   double rowLogSumExp_double[idxsType](ARGUMENTS_LIST)
 
  ARGUMENTS_LIST:
-  double *x, R_xlen_t nrow, R_xlen_t ncol, void *rows, R_xlen_t nrows, int rowsType, void *cols, R_xlen_t ncols, int colsType, int narm, int hasna, int byrow, double *ans, int cores
+  double *x, R_xlen_t nrow, R_xlen_t ncol, void *rows, R_xlen_t nrows, int rowsType, void *cols, R_xlen_t ncols, int colsType, int narm, int hasna, R_xlen_t byrow, double *ans, int cores
  ***********************************************************************/
 #include <pthread.h>
 #include <stdint.h>
@@ -127,7 +127,7 @@ RETURN_TYPE METHOD_NAME_IDXS(ARGUMENTS_LIST) {
   R_xlen_t ii, idx;
   double navalue;
   /* extern 1-D function 'logSumExp' */
-  extern double (*logSumExp_double[3])(double *x, void *idxs, R_xlen_t nidxs, int narm, int hasna, int by, double *xx);
+  extern double (*logsumexp)(double *x, void *idxs, R_xlen_t nidxs, int narm, int hasna, int by, double *xx);
 
 #ifdef IDXS_TYPE
   IDXS_C_TYPE *crows = (IDXS_C_TYPE*) rows;
@@ -139,26 +139,28 @@ RETURN_TYPE METHOD_NAME_IDXS(ARGUMENTS_LIST) {
        Should be freed manually. */
     double *xx = Calloc(ncols, double);
     navalue = (narm || ncols == 0) ? R_NegInf : NA_REAL;
+    logsumexp = logSumExp_double[colsType];
 
     for (ii=0; ii < nrows; ++ii) {
       idx = IDX_INDEX(crows,ii);
       if (idx == NA_R_XLEN_T) {
         ans[ii] = navalue;
       } else {
-        ans[ii] = logSumExp_double[colsType](x+idx, cols, ncols, narm, hasna, nrow, xx);
+        ans[ii] = logsumexp(x+idx, cols, ncols, narm, hasna, nrow, xx);
       }
     }
     Free(xx);
 
   } else {
     navalue = (narm || nrows == 0) ? R_NegInf : NA_REAL;
+    logsumexp = logSumExp_double[rowsType];
 
     for (ii=0; ii < ncols; ++ii) {
       idx = R_INDEX_OP(IDX_INDEX(ccols,ii), *, nrow);
       if (idx == NA_R_XLEN_T) {
         ans[ii] = navalue;
       } else {
-        ans[ii] = logSumExp_double[rowsType](x+idx, rows, nrows, narm, hasna, 0, NULL);
+        ans[ii] = logsumexp(x+idx, rows, nrows, narm, hasna, 0, NULL);
       }
     }
   } /* if (byrow) */
