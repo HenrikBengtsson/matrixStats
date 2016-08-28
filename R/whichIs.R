43d25e0e9771dc6b6dfcd5923551eef49dd16e70
hb <hb@aroma-project.org>
2016-08-27 20:41:11 -0700

    Renamed argument 'y' to 'value'

--- R/whichIs.R
+++ R/whichIs.R
@@ -11,7 +11,7 @@
 #
 # \arguments{
 #   \item{x}{A @numeric @vector of length K.}
-#   \item{y}{A @numeric scalar.}
+#   \item{value}{A @numeric scalar.}
 #   \item{op}{A @character string specifying the comparison to use.}
 #   \item{...}{Not used.}
 # }
@@ -21,74 +21,71 @@
 # }
 #
 # \details{
-#   \code{which(x, y, op) is more memory efficient than the corresponding
-#   \code{which(x OP y)} call, because the latter allocates a temporary
+#   \code{which(x, value, op)} is more memory efficient than the corresponding
+#   \code{which(x OP value)} call, because the latter allocates a temporary
 #   logical vector of length K whereas the former does not.  Moreover,
-#   if the types of \code{x} and \code{y} are such that \code{x} needs to
-#   be coerced to that of \code{y}, then another temporary vector of type
-#   \code{typeof(y)} and length K needs to be allocated.
+#   if the types of \code{x} and \code{value} are such that \code{x} needs to
+#   be coerced to that of \code{value}, then another temporary vector of type
+#   \code{typeof(value)} and length K needs to be allocated.
 # }
 #
 # @author "HB"
 #
 # @keyword "univar"
 #*/############################################################################
-whichIs <- function(x, y, op=c("==", "!=", "<", "<=", ">", ">="), ...) {
+whichIs <- function(x, value, op=c("==", "!=", "<", "<=", ">", ">="), ...) {
   nx <- length(x)
-  stopifnot(length(y) == 1)
+  stopifnot(length(value) == 1)
   op <- match.arg(op)
 
   ## Early stopping?
-  if (is.na(y)) return(integer(0L))
+  if (is.na(value)) return(integer(0L))
   
-  tx <- typeof(x)
-  ty <- typeof(y)
-
-  fcnName <- sprintf("whichIs_%s_%s", tx, ty)
+  fcnName <- sprintf("whichIs_%s_%s", typeof(x), typeof(value))
   ns <- asNamespace("matrixStats")
   if (exists(fcnName, mode="function", envir=ns)) {
     fcn <- get(fcnName, mode="function", envir=ns)
-    res <- fcn(x, y, op=op)
+    res <- fcn(x, value, op=op)
   } else {
     fcn <- get(op, mode="function", envir=baseenv())
-    res <- fcn(x, y)
+    res <- fcn(x, value)
     res <- which(res)
   }
   
   res
 } # whichIs()
 
-whichIs_integer_integer <- function(x, y, op) {
+whichIs_integer_integer <- function(x, value, op) {
   fcn <- get(op, mode="function", envir=baseenv())
-  res <- fcn(x, y)
+  res <- fcn(x, value)
   which(res)
 }
 
-whichIs_double_double <- function(x, y, op) {
+whichIs_double_double <- function(x, value, op) {
   fcn <- get(op, mode="function", envir=baseenv())
-  res <- fcn(x, y)
+  res <- fcn(x, value)
   which(res)
 }
 
-whichIs_integer_double <- function(x, y, op) {
+whichIs_integer_double <- function(x, value, op) {
   nx <- length(x)
   
   ## Early / instant stopping?
-  if (is.infinite(y)) {
+  if (is.infinite(value)) {
     if (op == "==") {
       return(integer(0L))
     } else if (op == "!=") {
       return(seq_len(nx))
     } else if (op == ">" || op == ">=") {
-      if (y > 0) {
+      if (value > 0) {
         return(integer(0L))
-      } else if (y < 0) {
+      } else if (value < 0) {
         return(seq_len(nx))
       }
     } else if (op == "<" || op == "<=") {
-      if (y > 0) {
+      if (value > 0) {
         return(seq_len(nx))
-      } else if (y < 0) {
+      } else if (value < 0) {
         return(integer(0L))
       }
     }
@@ -96,18 +93,20 @@ whichIs_integer_double <- function(x, y, op) {
 
   ## Early stopping?
   if (op == "==") {
-    if (y %% 1 != 0) return(integer(0L))
+    if (value %% 1 != 0) return(integer(0L))
   } else if (op == "!=") {
-    if (y %% 1 != 0) return(seq_len(nx))
+    if (value %% 1 != 0) return(seq_len(nx))
   }
 
   fcn <- get(op, mode="function", envir=baseenv())
-  res <- fcn(x, y)
+  x <- as.double(x)
+  res <- fcn(x, value)
   which(res)
 }
 
-whichIs_double_integer <- function(x, y, op) {
+whichIs_double_integer <- function(x, value, op) {
   fcn <- get(op, mode="function", envir=baseenv())
-  res <- fcn(x, y)
+  value <- as.double(value)
+  res <- fcn(x, value)
   which(res)
 }
