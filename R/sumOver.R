189d11ba38962053f86e0e91f45cd0d0099946df
hb <hb@aroma-project.org>
2017-03-28 22:02:23 -0700

    Rdoc -> Roxygen2

--- R/sumOver.R
+++ R/sumOver.R
@@ -1,54 +1,42 @@
-############################################################################/**
-# @RdocFunction sumOver
-#
-# @title "Fast sum over subset of vector elements"
-#
-# @synopsis
-#
-# \description{
-#   Computes the sum of all or a subset of values.
-# }
-#
-# \arguments{
-#   \item{x}{A @numeric @vector of length N.}
-#   \item{idxs}{A @vector indicating subset of elements
-#      to operate over. If @NULL, no subsetting is done.}
-#   \item{na.rm}{If @TRUE, missing values are skipped, otherwise not.}
-#   \item{mode}{A @character string specifying the data type of the
-#      return value.  Default is to use the same mode as argument
-#      \code{x}.}
-#   \item{...}{Not used.}
-# }
-#
-# \value{
-#   Returns a scalar of the data type specified by argument \code{mode}.
-#   If \code{mode="integer"}, then integer overflow occurs if the
-#   \emph{sum} is outside the range of defined integer values.
-# }
-#
-# \details{
-#   \code{sumOver(x, idxs)} gives equivalent results as
-#   \code{sum(x[idxs])}, but is faster and more memory efficient
-#   since it avoids the actual subsetting which requires copying
-#   of elements and garbage collection thereof.
-#
-#   Furthermore, \code{sumOver(x, mode="double")} is equivalent to
-#   \code{sum(as.numeric(x))}, but is much more memory efficient when
-#   \code{x} is an @integer vector.
-# }
-#
-# @examples "../incl/sumOver.Rex"
-#
-# \seealso{
-#   @see "base::sum".
-#   To efficiently average over a subset, see @see "meanOver".
-# }
-#
-# @author
-#
-# @keyword univar
-# @keyword internal
-#*/############################################################################
+#' Fast sum over subset of vector elements
+#' 
+#' Computes the sum of all or a subset of values.
+#' 
+#' \code{sumOver(x, idxs)} gives equivalent results as \code{sum(x[idxs])}, but
+#' is faster and more memory efficient since it avoids the actual subsetting
+#' which requires copying of elements and garbage collection thereof.
+#' 
+#' Furthermore, \code{sumOver(x, mode="double")} is equivalent to
+#' \code{sum(as.numeric(x))}, but is much more memory efficient when \code{x}
+#' is an \code{\link[base]{integer}} vector.
+#' 
+#' @param x A \code{\link[base]{numeric}} \code{\link[base]{vector}} of length
+#' N.
+#' 
+#' @param idxs A \code{\link[base]{vector}} indicating subset of elements to
+#' operate over. If \code{\link[base]{NULL}}, no subsetting is done.
+#' 
+#' @param na.rm If \code{\link[base:logical]{TRUE}}, missing values are
+#' skipped, otherwise not.
+#' 
+#' @param mode A \code{\link[base]{character}} string specifying the data type
+#' of the return value.  Default is to use the same mode as argument \code{x}.
+#' 
+#' @param ... Not used.
+#' 
+#' @return Returns a scalar of the data type specified by argument \code{mode}.
+#' If \code{mode="integer"}, then integer overflow occurs if the \emph{sum} is
+#' outside the range of defined integer values.
+#' 
+#' @example incl/sumOver.Rex
+#'
+#' @author Henrik Bengtsson
+#' 
+#' @seealso \code{\link[base]{sum}}().  To efficiently average over a subset,
+#' see \code{\link{meanOver}}().
+#' 
+#' @keywords univar internal
+#' @export
 sumOver <- function(x, idxs=NULL, na.rm=FALSE, mode=typeof(x), ...) {
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   # Validate arguments
