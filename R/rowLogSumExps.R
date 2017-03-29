907f2d60de48b36449f43cd1e98fb89e2979d265
hb <hb@aroma-project.org>
2017-03-28 21:40:51 -0700

    Rdoc -> Roxygen2

--- R/rowLogSumExps.R
+++ R/rowLogSumExps.R
@@ -1,51 +1,40 @@
-###########################################################################/**
-# @RdocFunction rowLogSumExps
-# @alias colLogSumExps
-# \alias{rowLogSumExps,matrix-method}
-# \alias{colLogSumExps,matrix-method}
-#
-# @title "Accurately computes the logarithm of the sum of exponentials across rows or columns"
-#
-# \description{
-#   @get "title".
-# }
-#
-# \usage{
-#  @usage rowLogSumExps
-#  @usage colLogSumExps
-# }
-#
-# \arguments{
-#  \item{lx}{A @numeric NxK @matrix.
-#   Typically \code{lx} are \eqn{log(x)} values.}
-#  \item{rows, cols}{A @vector indicating subset of rows (and/or columns)
-#   to operate over. If @NULL, no subsetting is done.}
-#  \item{na.rm}{If @TRUE, any missing values are ignored, otherwise not.}
-#  \item{dim.}{An @integer @vector of length two specifying the
-#               dimension of \code{x}, also when not a @matrix.}
-#  \item{...}{Not used.}
-# }
-#
-# \value{
-#  A @numeric @vector of length N (K).
-# }
-#
-# \section{Benchmarking}{
-#   These methods are implemented in native code and have been optimized
-#   for speed and memory.
-# }
-#
-# \author{
-#   Native implementation by Henrik Bengtsson.
-#   Original R code by Nakayama ??? (Japan).
-# }
-#
-# \seealso{
-#   To calculate the same on vectors, @see "logSumExp".
-# }
-#
-# @keyword array
-#*/###########################################################################
+#' Accurately computes the logarithm of the sum of exponentials across rows or
+#' columns
+#' 
+#' Accurately computes the logarithm of the sum of exponentials across rows or
+#' columns.
+#' 
+#' 
+#' @param lx A \code{\link[base]{numeric}} NxK \code{\link[base]{matrix}}.
+#' Typically \code{lx} are \eqn{log(x)} values.
+#' 
+#' @param rows,cols A \code{\link[base]{vector}} indicating subset of rows
+#' (and/or columns) to operate over. If \code{\link[base]{NULL}}, no subsetting
+#' is done.
+#' 
+#' @param na.rm If \code{\link[base:logical]{TRUE}}, any missing values are
+#' ignored, otherwise not.
+#' 
+#' @param dim. An \code{\link[base]{integer}} \code{\link[base]{vector}} of
+#' length two specifying the dimension of \code{x}, also when not a
+#' \code{\link[base]{matrix}}.
+#' 
+#' @param ... Not used.
+#' 
+#' @return A \code{\link[base]{numeric}} \code{\link[base]{vector}} of length N
+#' (K).
+#' 
+#' @section Benchmarking:
+#' These methods are implemented in native code and have been optimized for speed
+#' and memory.
+#' 
+#' @author Native implementation by Henrik Bengtsson.  Original R code by
+#' Nakayama ??? (Japan).
+#' 
+#' @seealso To calculate the same on vectors, \code{\link{logSumExp}}().
+#' 
+#' @keywords array
+#' @export
 rowLogSumExps <- function(lx, rows=NULL, cols=NULL, na.rm=FALSE, dim.=dim(lx), ...) {
   dim. <- as.integer(dim.)
   hasNA <- TRUE;
@@ -63,6 +52,8 @@ rowLogSumExps <- function(lx, rows=NULL, cols=NULL, na.rm=FALSE, dim.=dim(lx), .
 }
 
 
+#' @rdname rowLogSumExps
+#' @export
 colLogSumExps <- function(lx, rows=NULL, cols=NULL, na.rm=FALSE, dim.=dim(lx), ...) {
   dim. <- as.integer(dim.)
   hasNA <- TRUE;
