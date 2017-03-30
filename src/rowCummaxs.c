a83000accbd0c5f3dbadc0b6a56b19399a3c2903
hb <hb@aroma-project.org>
2017-03-29 17:26:41 -0700

    CODE HARMONIZATION: Split up rowCumMinMaxs.c into rowCummins.c and rowCummaxs.c

---
+++ src/rowCummaxs.c
@@ -0,0 +1,91 @@
+/***************************************************************************
+ Public methods:
+ SEXP rowCummaxs(SEXP x, ...)
+ SEXP colCummaxs(SEXP x, ...)
+
+ Authors: Henrik Bengtsson
+
+ Copyright Henrik Bengtsson, 2014
+ **************************************************************************/
+#include <Rdefines.h>
+#include "types.h"
+#include "utils.h"
+
+/*
+Native API (dynamically generated via macros):
+
+void rowCummaxs_Integer_noRows_noCols(int *x, R_xlen_t nrow, R_xlen_t ncol, void *rows, R_xlen_t Rf_nrows, void *cols, R_xlen_t Rf_ncols, int byrow, int *ans)
+void rowCummaxs_Integer_noRows_intCols(int *x, R_xlen_t nrow, R_xlen_t ncol, void *rows, R_xlen_t Rf_nrows, void *cols, R_xlen_t Rf_ncols, int byrow, int *ans)
+void rowCummaxs_Integer_noRows_realCols(int *x, R_xlen_t nrow, R_xlen_t ncol, void *rows, R_xlen_t Rf_nrows, void *cols, R_xlen_t Rf_ncols, int byrow, int *ans)
+void rowCummaxs_Integer_intRows_noCols(int *x, R_xlen_t nrow, R_xlen_t ncol, void *rows, R_xlen_t Rf_nrows, void *cols, R_xlen_t Rf_ncols, int byrow, int *ans)
+void rowCummaxs_Integer_intRows_intCols(int *x, R_xlen_t nrow, R_xlen_t ncol, void *rows, R_xlen_t Rf_nrows, void *cols, R_xlen_t Rf_ncols, int byrow, int *ans)
+void rowCummaxs_Integer_intRows_realCols(int *x, R_xlen_t nrow, R_xlen_t ncol, void *rows, R_xlen_t Rf_nrows, void *cols, R_xlen_t Rf_ncols, int byrow, int *ans)
+void rowCummaxs_Integer_realRows_noCols(int *x, R_xlen_t nrow, R_xlen_t ncol, void *rows, R_xlen_t Rf_nrows, void *cols, R_xlen_t Rf_ncols, int byrow, int *ans)
+void rowCummaxs_Integer_realRows_intCols(int *x, R_xlen_t nrow, R_xlen_t ncol, void *rows, R_xlen_t Rf_nrows, void *cols, R_xlen_t Rf_ncols, int byrow, int *ans)
+void rowCummaxs_Integer_realRows_realCols(int *x, R_xlen_t nrow, R_xlen_t ncol, void *rows, R_xlen_t Rf_nrows, void *cols, R_xlen_t Rf_ncols, int byrow, int *ans)
+void rowCummaxs_Real_noRows_noCols(double *x, R_xlen_t nrow, R_xlen_t ncol, void *rows, R_xlen_t Rf_nrows, void *cols, R_xlen_t Rf_ncols, int byrow, double *ans)
+void rowCummaxs_Real_noRows_intCols(double *x, R_xlen_t nrow, R_xlen_t ncol, void *rows, R_xlen_t Rf_nrows, void *cols, R_xlen_t Rf_ncols, int byrow, double *ans)
+void rowCummaxs_Real_noRows_realCols(double *x, R_xlen_t nrow, R_xlen_t ncol, void *rows, R_xlen_t Rf_nrows, void *cols, R_xlen_t Rf_ncols, int byrow, double *ans)
+void rowCummaxs_Real_intRows_noCols(double *x, R_xlen_t nrow, R_xlen_t ncol, void *rows, R_xlen_t Rf_nrows, void *cols, R_xlen_t Rf_ncols, int byrow, double *ans)
+void rowCummaxs_Real_intRows_intCols(double *x, R_xlen_t nrow, R_xlen_t ncol, void *rows, R_xlen_t Rf_nrows, void *cols, R_xlen_t Rf_ncols, int byrow, double *ans)
+void rowCummaxs_Real_intRows_realCols(double *x, R_xlen_t nrow, R_xlen_t ncol, void *rows, R_xlen_t Rf_nrows, void *cols, R_xlen_t Rf_ncols, int byrow, double *ans)
+void rowCummaxs_Real_realRows_noCols(double *x, R_xlen_t nrow, R_xlen_t ncol, void *rows, R_xlen_t Rf_nrows, void *cols, R_xlen_t Rf_ncols, int byrow, double *ans)
+void rowCummaxs_Real_realRows_intCols(double *x, R_xlen_t nrow, R_xlen_t ncol, void *rows, R_xlen_t Rf_nrows, void *cols, R_xlen_t Rf_ncols, int byrow, double *ans)
+void rowCummaxs_Real_realRows_realCols(double *x, R_xlen_t nrow, R_xlen_t ncol, void *rows, R_xlen_t Rf_nrows, void *cols, R_xlen_t Rf_ncols, int byrow, double *ans)
+*/
+
+#define RETURN_TYPE void
+#define ARGUMENTS_LIST X_C_TYPE *x, R_xlen_t nrow, R_xlen_t ncol, void *rows, R_xlen_t nrows, void *cols, R_xlen_t ncols, int byrow, ANS_C_TYPE *ans
+
+#define METHOD rowCummaxs
+#define COMP '>'
+#define METHOD_TEMPLATE_H "rowCumMinMaxs_TYPE-template.h"
+#define X_TYPE 'i'
+#include "templates-gen-matrix.h"
+#define X_TYPE 'r'
+#include "templates-gen-matrix.h"
+
+
+SEXP rowCummaxs(SEXP x, SEXP dim, SEXP rows, SEXP cols, SEXP byRow) {
+  int byrow;
+  SEXP ans = NILSXP;
+  R_xlen_t nrow, ncol;
+
+  /* Argument 'x' and 'dim': */
+  assertArgMatrix(x, dim, (R_TYPE_INT | R_TYPE_REAL), "x");
+  nrow = asR_xlen_t(dim, 0);
+  ncol = asR_xlen_t(dim, 1);
+
+  /* Argument 'rows' and 'cols': */
+  R_xlen_t nrows, ncols;
+  int rowsType, colsType;
+  void *crows = validateIndices(rows, nrow, 0, &nrows, &rowsType);
+  void *ccols = validateIndices(cols, ncol, 0, &ncols, &colsType);
+
+  /* Argument 'byRow': */
+  byrow = asLogical(byRow);
+
+  /* Double matrices are more common to use. */
+  if (isReal(x)) {
+    PROTECT(ans = allocMatrix(REALSXP, nrows, ncols));
+    rowCummaxs_Real[rowsType][colsType](REAL(x), nrow, ncol, crows, nrows, ccols, ncols, byrow, REAL(ans));
+    UNPROTECT(1);
+  } else if (isInteger(x)) {
+    PROTECT(ans = allocMatrix(INTSXP, nrows, ncols));
+    rowCummaxs_Integer[rowsType][colsType](INTEGER(x), nrow, ncol, crows, nrows, ccols, ncols, byrow, INTEGER(ans));
+    UNPROTECT(1);
+  }
+
+  return(ans);
+} /* rowCummaxs() */
+
+#undef COMP
+
+
+/***************************************************************************
+ HISTORY:
+ 2015-06-07 [DJ]
+  o Supported subsetted computation.
+ 2014-11-26 [HB]
+ o Created from rowVars.c.
+ **************************************************************************/
