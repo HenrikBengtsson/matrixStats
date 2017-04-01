b868ec4f06fc3cd5c4a63d4e225b6c6fc1881340
hb <hb@aroma-project.org>
2017-04-01 15:54:14 -0700

    LINT: add spacing

--- inst/benchmarking/R/random-matrices.R
+++ inst/benchmarking/R/random-matrices.R
@@ -1,31 +1,31 @@
-rmatrix <- function(nrow, ncol, mode=c("logical", "double", "integer", "index"), range=c(-100,+100), naProb=0) {
+rmatrix <- function(nrow, ncol, mode = c("logical", "double", "integer", "index"), range = c(-100, +100), naProb = 0) {
   mode <- match.arg(mode)
   n <- nrow*ncol
   if (mode == "logical") {
-    X <- sample(c(FALSE, TRUE), size=n, replace=TRUE)
+    X <- sample(c(FALSE, TRUE), size = n, replace = TRUE)
   } else if (mode == "index") {
     X <- seq_len(n)
     mode <- "integer"
   } else {
-    X <- runif(n, min=range[1], max=range[2])
+    X <- runif(n, min = range[1], max = range[2])
   }
   storage.mode(X) <- mode
-  if (naProb > 0) X[sample(n, size=naProb*n)] <- NA
+  if (naProb > 0) X[sample(n, size = naProb*n)] <- NA
   dim(X) <- c(nrow, ncol)
   X
 } # rmatrix()
 
 
-rmatrices <- function(scale=10, seed=1, ...) {
+rmatrices <- function(scale = 10, seed = 1, ...) {
   set.seed(seed)
   data <- list()
-  data[[1]] <- rmatrix(nrow=scale*  1, ncol=scale*  1, ...)
-  data[[2]] <- rmatrix(nrow=scale* 10, ncol=scale* 10, ...)
-  data[[3]] <- rmatrix(nrow=scale*100, ncol=scale*  1, ...)
+  data[[1]] <- rmatrix(nrow = scale*  1, ncol = scale*  1, ...)
+  data[[2]] <- rmatrix(nrow = scale* 10, ncol = scale* 10, ...)
+  data[[3]] <- rmatrix(nrow = scale*100, ncol = scale*  1, ...)
   data[[4]] <- t(data[[3]])
-  data[[5]] <- rmatrix(nrow=scale* 10, ncol=scale*100, ...)
+  data[[5]] <- rmatrix(nrow = scale* 10, ncol = scale*100, ...)
   data[[6]] <- t(data[[5]])
-  names(data) <- sapply(data, FUN=function(x) paste(dim(x), collapse="x"))
+  names(data) <- sapply(data, FUN = function(x) paste(dim(x), collapse = "x"))
   data
 } # rmatrices()
 
