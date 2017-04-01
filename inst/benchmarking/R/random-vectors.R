8b9feba5117c8bb92d8f659e4e868dba17f3ac69
hb <hb@aroma-project.org>
2017-04-01 15:57:49 -0700

    LINT: Space around arith operators

--- inst/benchmarking/R/random-vectors.R
+++ inst/benchmarking/R/random-vectors.R
@@ -1,12 +1,12 @@
 rvector <- function(n, mode = c("logical", "double", "integer"), range = c(-100, +100), naProb = 0) {
   mode <- match.arg(mode)
   if (mode == "logical") {
-    X <- sample(c(FALSE, TRUE), size = n, replace = TRUE)
+    x <- sample(c(FALSE, TRUE), size = n, replace = TRUE)
   } else {
     x <- runif(n, min = range[1], max = range[2])
   }
   storage.mode(x) <- mode
-  if (naProb > 0) x[sample(n, size = naProb*n)] <- NA
+  if (naProb > 0) x[sample(n, size = naProb * n)] <- NA
   x
 } # rvector()
 
@@ -14,11 +14,11 @@ rvector <- function(n, mode = c("logical", "double", "integer"), range = c(-100,
 rvectors <- function(scale = 10, seed = 1, ...) {
   set.seed(seed)
   data <- list()
-  data[[1]] <- rvector(n = scale*1e2, ...)
-  data[[2]] <- rvector(n = scale*1e3, ...)
-  data[[3]] <- rvector(n = scale*1e4, ...)
-  data[[4]] <- rvector(n = scale*1e5, ...)
-  data[[5]] <- rvector(n = scale*1e6, ...)
+  data[[1]] <- rvector(n = scale * 1e2, ...)
+  data[[2]] <- rvector(n = scale * 1e3, ...)
+  data[[3]] <- rvector(n = scale * 1e4, ...)
+  data[[4]] <- rvector(n = scale * 1e5, ...)
+  data[[5]] <- rvector(n = scale * 1e6, ...)
   names(data) <- sprintf("n = %d", sapply(data, FUN = length))
   data
 } # rvectors()
