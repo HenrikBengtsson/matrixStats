39b6a1b52c3983a32695fc16e1ed81bc2cb13222
hb <hb@aroma-project.org>
2017-04-01 17:28:14 -0700

    Package is now silent when loaded; it no longer displays a startup message

--- R/zzz.R
+++ R/zzz.R
@@ -2,12 +2,3 @@
 .onUnload <- function(libpath) {
   library.dynam.unload("matrixStats", libpath)
 }
-
-.onAttach <- function(libname, pkgname) {
-  ## covr: skip=5
-  pd <- utils::packageDescription(pkgname)
-  msg <- sprintf("%s v%s", pkgname, pd$Version)
-  if (!is.null(pd$Date)) msg <- sprintf("%s (%s)", msg, pd$Date)
-  msg <- sprintf("%s successfully loaded. See ?%s for help.", msg, pkgname)
-  pkgStartupMessage(msg)
-}
