555ef5f8b927e33c805ede4020f2be9bdfcf4961
hb <hb@aroma-project.org>
2017-04-01 17:40:19 -0700

    Functions colDiffs() and rowDiffs() gained argument 'dim.' [#83]

--- R/rowDiffs.R
+++ R/rowDiffs.R
@@ -14,6 +14,10 @@
 #' @param differences An \code{\link[base]{integer}} specifying the order of
 #' difference.
 #'
+#' @param dim. An \code{\link[base]{integer}} \code{\link[base]{vector}} of
+#' length two specifying the dimension of \code{x}, also when not a
+#' \code{\link[base]{matrix}}.
+#' 
 #' @param ... Not used.
 #'
 #' @return Returns a \code{\link[base]{numeric}} Nx(K-1) or (N-1)xK
@@ -27,15 +31,17 @@
 #' @keywords array iteration robust univar
 #' @export
 rowDiffs <- function(x, rows = NULL, cols = NULL,
-                     lag = 1L, differences = 1L, ...) {
-  .Call(C_rowDiffs, x, dim(x), rows, cols,
+                     lag = 1L, differences = 1L, dim. = dim(x), ...) {
+  dim <- as.integer(dim.)
+  .Call(C_rowDiffs, x, dim., rows, cols,
         as.integer(lag), as.integer(differences), TRUE)
 }
 
 #' @rdname rowDiffs
 #' @export
 colDiffs <- function(x, rows = NULL, cols = NULL,
-                     lag = 1L, differences = 1L, ...) {
-  .Call(C_rowDiffs, x, dim(x), rows, cols,
+                     lag = 1L, differences = 1L, dim. = dim(x), ...) {
+  dim <- as.integer(dim.)
+  .Call(C_rowDiffs, x, dim., rows, cols,
         as.integer(lag), as.integer(differences), FALSE)
 }
