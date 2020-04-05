5dc36ec8734993c9db5ed7244a9042edd3eb6cdc
Henrik Bengtsson <hb@aroma-project.org>
2020-04-05 08:38:54 -0700

    TESTS: Add tests for Bug #170

--- tests/rowCollapse_subset.R
+++ tests/rowCollapse_subset.R
@@ -16,21 +16,24 @@ rowCollapse_R <- function(x, idxs, ...) {
 source("utils/validateIndicesFramework.R")
 x <- matrix(runif(6 * 6, min = -6, max = 6), nrow = 6, ncol = 6)
 storage.mode(x) <- "integer"
-idxs <- seq_len(6)
 for (rows in index_cases) {
   if (is.null(rows)) rows <- seq_len(nrow(x))
 
-  suppressWarnings({
-    actual <- tryCatch(rowCollapse(x, idxs, rows = rows),
-                       error = function(c) "error")
-    expect <- tryCatch(rowCollapse_R(x[rows, , drop = FALSE], idxs[rows]),
-                       error = function(c) "error")
-  })
-  stopifnot(all.equal(actual, expect))
-
-  suppressWarnings({
-    actual <- tryCatch(colCollapse(t(x), idxs, cols = rows),
-                       error = function(c) "error")
-  })
-  stopifnot(all.equal(actual, expect))
+  for (idxs in list(2L, seq_len(6L))) {
+    suppressWarnings({
+      actual <- tryCatch(rowCollapse(x, idxs, rows = rows),
+                         error = function(c) "error")
+      expect <- tryCatch({
+        idxs_0 <- rep(idxs, length.out = nrow(x))[rows]
+        rowCollapse_R(x[rows, , drop = FALSE], idxs_0)
+      }, error = function(c) "error")
+    })
+    stopifnot(all.equal(actual, expect))
+  
+    suppressWarnings({
+      actual <- tryCatch(colCollapse(t(x), idxs, cols = rows),
+                         error = function(c) "error")
+    })
+    stopifnot(all.equal(actual, expect))
+  }
 }
