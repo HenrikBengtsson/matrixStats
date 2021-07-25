47e98b60b50fb05e07db4ea885f7f114853e749b
AngelPn <aggi.pa@gmail.com>
2021-07-25 21:54:50 +0200

    PERFORMANCE: Avoid all.equal() in package code

--- R/rowAlls.R
+++ R/rowAlls.R
@@ -87,7 +87,7 @@ rowAlls <- function(x, rows = NULL, cols = NULL, value = TRUE,
     } else {
       z <- (x == value)
       dim <- dim(x) # for 0xN and Mx0 cases; needed in R (< 3.4.0)
-      if (!isTRUE(all.equal(dim(z), dim))) dim(z) <- dim
+      if (!identical(dim(z), as.integer(dim))) dim(z) <- dim
       if (isTRUE(useNames)) dimnames(z) <- dimnames(x)
       return(rowAlls(z, na.rm = na.rm, dim. = dim., ..., useNames = useNames))
     }
@@ -148,7 +148,7 @@ colAlls <- function(x, rows = NULL, cols = NULL, value = TRUE,
     } else {
       z <- (x == value)
       dim <- dim(x) # for 0xN and Mx0 cases; needed in R (< 3.4.0)
-      if (!isTRUE(all.equal(dim(z), dim))) dim(z) <- dim
+      if (!identical(dim(z), as.integer(dim))) dim(z) <- dim
       if (isTRUE(useNames)) dimnames(z) <- dimnames(x)
       return(colAlls(z, na.rm = na.rm, dim. = dim., ..., useNames = useNames))
     }
@@ -240,7 +240,7 @@ rowAnys <- function(x, rows = NULL, cols = NULL, value = TRUE,
     } else {
       z <- (x == value)
       dim <- dim(x) # for 0xN and Mx0 cases; needed in R (< 3.4.0)
-      if (!isTRUE(all.equal(dim(z), dim))) dim(z) <- dim
+      if (!identical(dim(z), as.integer(dim))) dim(z) <- dim
       if (isTRUE(useNames)) dimnames(z) <- dimnames(x)
       return(rowAnys(z, na.rm = na.rm, dim. = dim., ..., useNames = useNames))
     }
@@ -301,7 +301,7 @@ colAnys <- function(x, rows = NULL, cols = NULL, value = TRUE,
     } else {
       z <- (x == value)
       dim <- dim(x) # for 0xN and Mx0 cases; needed in R (< 3.4.0)
-      if (!isTRUE(all.equal(dim(z), dim))) dim(z) <- dim
+      if (!identical(dim(z), as.integer(dim))) dim(z) <- dim
       if (isTRUE(useNames)) dimnames(z) <- dimnames(x)
       return(colAnys(z, na.rm = na.rm, dim. = dim., ..., useNames = useNames))
     }
