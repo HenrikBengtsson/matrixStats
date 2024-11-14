0f97073c7fa24bb11f48facb77778371e313f36a
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2024-11-13 23:27:42 -0800

    undo indentation

--- src/diff2_lowlevel_template.h
+++ src/diff2_lowlevel_template.h
@@ -70,7 +70,7 @@ void CONCAT_MACROS(diff2, X_C_SIGNATURE)(X_C_TYPE *x, R_xlen_t nx,
       
       ans[ii] = X_DIFF(xvalue2, xvalue1); 
     }
-    } else {
+  } else {
     /* Allocate temporary work vector (to hold intermediate differences) */
     tmp = R_Calloc(nidxs - lag, X_C_TYPE);
 
