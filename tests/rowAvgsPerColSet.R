fb772d9d555120f99b147f54624f8095bfc41544
Maximilian Muecke <muecke.maximilian@gmail.com>
2026-06-16 17:45:54 +0200

    test: derive expected values from 'S' in rowAvgsPerColSet weight tests

--- tests/rowAvgsPerColSet.R
+++ tests/rowAvgsPerColSet.R
@@ -88,16 +88,16 @@ print(Z2)
 # - - - - - - - - - - - - - - - - - - - - - - - - - -
 rowWMean <- function(x, W, ...) rowSums(x * W) / rowSums(W)
 Z <- rowAvgsPerColSet(X, W = W, S = S, FUN = rowWMean)
-Z0 <- cbind(s1 = rowWMean(X[, 1:2], W[, 1:2]),
-            s2 = rowWMean(X[, 3:4], W[, 3:4]),
-            s3 = rowWMean(X[, 5:6], W[, 5:6]))
+Z0 <- cbind(s1 = rowWMean(X[, S[, 1L]], W[, S[, 1L]]),
+            s2 = rowWMean(X[, S[, 2L]], W[, S[, 2L]]),
+            s3 = rowWMean(X[, S[, 3L]], W[, S[, 3L]]))
 stopifnot(identical(drop(Z), Z0))
 
 colWMean <- function(x, W, ...) colSums(x * W) / colSums(W)
 Z <- colAvgsPerRowSet(X, W = W, S = S, FUN = colWMean)
-Z0 <- rbind(s1 = colWMean(X[1:2, ], W[1:2, ]),
-            s2 = colWMean(X[3:4, ], W[3:4, ]),
-            s3 = colWMean(X[5:6, ], W[5:6, ]))
+Z0 <- rbind(s1 = colWMean(X[S[, 1L], ], W[S[, 1L], ]),
+            s2 = colWMean(X[S[, 2L], ], W[S[, 2L], ]),
+            s3 = colWMean(X[S[, 3L], ], W[S[, 3L], ]))
 stopifnot(identical(drop(Z), Z0))
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - -
